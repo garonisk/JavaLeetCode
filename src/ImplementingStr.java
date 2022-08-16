7c401f589508107ffb8c143b5e389572ba674b78
zesik1 <kongaronis@gmail.com>
2022-08-16 16:27:55 +0300

    ImplementingStr

---
+++ src/ImplementingStr.java
@@ -0,0 +1,9 @@
+public class ImplementingStr {
+    public int strStr(String haystack, String needle) {
+        if (haystack.indexOf(needle) == -1) {
+            return -1;
+        } else {
+            return haystack.indexOf(needle);
+        }
+    }
+}
